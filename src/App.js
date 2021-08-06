d9629b48490faf5fb6f5d90be3f3ff1d8684bc80
Young <k2040458@kcl.ac.uk>
2021-08-06 11:42:35 +0100

    finished lesson 6

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import NewExpense from "./components/NewExpense/NewExpense";
 import Expenses from "./components/Expenses/Expenses";
 
+const DUMMY_EXPENSES = [
+  { date: new Date(2021, 2, 20), title: "Desk", amount: 75.99, id: "e1" },
+  {
+    date: new Date(2021, 2, 11),
+    title: "Breakfast",
+    amount: 10.99,
+    id: "e2",
+  },
+  {
+    date: new Date(2021, 4, 23),
+    title: "Not coffee",
+    amount: 2.36,
+    id: "e3",
+  },
+  { date: new Date(2021, 5, 10), title: "Alcohol", amount: 250.5, id: "e4" },
+];
+
 function App() {
-  const expenses = [
-    { date: new Date(2021, 2, 20), title: "Desk", amount: 75.99, id: "e1" },
-    {
-      date: new Date(2021, 2, 11),
-      title: "Breakfast",
-      amount: 10.99,
-      id: "e2",
-    },
-    {
-      date: new Date(2021, 4, 23),
-      title: "Not coffee",
-      amount: 2.36,
-      id: "e3",
-    },
-    { date: new Date(2021, 5, 10), title: "Alcohol", amount: 250.5, id: "e4" },
-  ];
+  // eslint-disable-next-line no-unused-vars
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const onAddNewExpenseHandler = (newExpense) => {
-    const expense = { ...newExpense };
-    console.log("in App JS", expense);
+    setExpenses((prevExpenses) => {
+      return [newExpense, ...prevExpenses];
+    });
   };
 
   return (
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../Card/Card";
 import ExpenseItem from "./ExpenseItem/ExpenseItem";
+import ExpensesFilter from "./ExpensesFilter/ExpensesFilter";
 import "./Expenses.css";
 
 const Expenses = (props) => {
+  const [yearFilter, setYearFilter] = useState("2020");
   const mappedExpenses = props.items.map((expense) => (
     <ExpenseItem
       key={expense.id}
@@ -12,7 +14,20 @@ const Expenses = (props) => {
       amount={expense.amount}
     />
   ));
-  return <Card className="expenses">{mappedExpenses}</Card>;
+
+  const onFilterChangeHandler = (selectedYear) => {
+    console.log("in Expenses.js", selectedYear);
+    setYearFilter(selectedYear);
+  };
+  return (
+    <Card className="expenses">
+      <ExpensesFilter
+        onFilterChange={onFilterChangeHandler}
+        selectedYear={yearFilter}
+      />
+      {mappedExpenses}
+    </Card>
+  );
 };
 
 export default Expenses;
